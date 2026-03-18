77f3a555a990bb4584d74a46f30663487e5bb6e3
Father-Pucci <rasonabe.mark2405@gmail.com>
2026-03-18 22:44:04 +0800

    Signout and data logged clearing

---
+++ auth/signout.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../config/functions.php';
+
+if (isLoggedIn()) {
+    logActivity($pdo, $_SESSION['user_id'], $_SESSION['email'], 'User signed out', 'logout');
+}
+session_unset();
+session_destroy();
+header('Location: /index.php');
+exit;
